77d100da826c2f9e507a8d1fa4fff2d22d375d2e
prabhsimar-s-sawhney <66642248+prabhsimar-s-sawhney@users.noreply.github.com>
2022-04-05 11:34:24 +0530

    deleted django files

--- README.md
+++
@@ -1,5 +0,0 @@
-# tennis-warehouse
-1. Install django.
-2. Install mysql workbench and shell.
-3. Add data according to models.
-4. You're good to go!!
--- ecommerce/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from ecommerce.models import Products, Vendors, Contacts
-# Register your models here.
-admin.site.register(Products)
-admin.site.register(Vendors)
-admin.site.register(Contacts)
